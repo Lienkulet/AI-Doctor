7435ee7c69292c21176a1bbfaf50851740b12d86
Lienkulet <clocicovalexandru@gmail.com>
2025-03-28 23:11:49 +0200

    fix

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
-export const indexName = "index-one"
+export const indexName = "doctor"
 export const namespace = "diagnosis2"
 export const topK = 5
 export const modelname = 'mixedbread-ai/mxbai-embed-large-v1'
